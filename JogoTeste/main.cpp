a473dc73a454df05e13c64887258b1d6181212fe
Andrey Catarin <andreycatarin@outlook.com>
2024-08-13 22:42:57 -0300

    teste

--- JogoTeste/main.cpp
+++ JogoTeste/main.cpp
@@ -3,8 +3,13 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RectangleShape shape(sf::Vector2f(100.f, 100.f));
+
+
     shape.setFillColor(sf::Color::Green);
+    shape.setOutlineThickness(21); 
+    shape.setOutlineColor(sf::Color::Cyan);
+
 
     while (window.isOpen())
     {
@@ -14,10 +19,10 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
         window.draw(shape);
         window.display();
+
     }
 
     return 0;
