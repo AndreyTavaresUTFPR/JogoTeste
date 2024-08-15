f3419d497dc1191b556cd1f4d4bc595ae1f10045
Andrey Catarin <andreycatarin@outlook.com>
2024-08-15 17:00:29 -0300

    Movimento aleatório

--- JogoTeste/Inimigo.cpp
+++ JogoTeste/Inimigo.cpp
@@ -2,7 +2,7 @@
 
 
 Inimigo::Inimigo(Jogador* jogador):
-	Personagem(), vida(VIDA_INIMIGO), vel(VEL_INIMIGO_X, VEL_INIMIGO_Y), jogador(jogador)
+	Personagem(), vida(VIDA_INIMIGO), vel(VEL_INIMIGO_X, VEL_INIMIGO_Y), jogador(jogador), moveAleatorio(rand()%5)
 {
 	body.setFillColor(sf::Color::Magenta);
 	body.setPosition(sf::Vector2f(400.f, 250.f));
@@ -31,21 +31,51 @@ void Inimigo::perseguir(sf::Vector2f posJogador, sf::Vector2f posInimigo) // Per
 }
 
 
-void Inimigo::move()
+void Personagens::Inimigo::movimentoAleatorio() // Movimento aleatório do inimigo
+{
+	if (moveAleatorio == 0) {
+		body.move(0.f, -vel.y/2.f);
+	}
+	else if (moveAleatorio == 1) {
+		body.move(vel.x/2.f, 0.f);
+	}
+	else if (moveAleatorio == 2) {
+		body.move(0.0f, vel.y/2.f);
+	} else if (moveAleatorio == 3) {
+		body.move(-vel.x/2.f, 0.f);
+	}
+	else {
+		body.move(0.f, 0.f);  // Pausas ocasionais no movimento
+	}
+
+	float tempo = relogio.getElapsedTime().asSeconds();
+	if (tempo >= 1.0f) {	// Relogio que controla o movimento aleatorio
+		moveAleatorio = rand() % 5;
+		relogio.restart();
+	}
+}
+
+
+void Inimigo::move() // Gerencia todo o movimento do inimigo
 {
 	sf::Vector2f posJogador = jogador->getBody().getPosition();
 	sf::Vector2f posInimigo = getBody().getPosition();
 
-	if (fabs(posJogador.x - posInimigo.x) <= RAIO_AGGRO_X &&
+	if (fabs(posJogador.x - posInimigo.x) <= RAIO_AGGRO_X &&  // Ve se o jogador está dentro do aggro
 		fabs(posJogador.y - posInimigo.y) <= RAIO_AGGRO_Y)   {
 
 			perseguir(posJogador, posInimigo);
+
+			moveAleatorio = 4; // Para o inimigo após sair do aggro
+			relogio.restart();
+
+	} else {
+		movimentoAleatorio();
 	}
-	// mvimento Aleatorio; à implementar
 }
 
 
-void Inimigo::executar()
+void Inimigo::executar() // Move o inimigo corretamente
 {
 	move();
 }
--- JogoTeste/Inimigo.h
+++ JogoTeste/Inimigo.h
@@ -4,8 +4,8 @@
 
 #define VIDA_INIMIGO 100
 
-#define VEL_INIMIGO_X 0.025f
-#define VEL_INIMIGO_Y 0.025f
+#define VEL_INIMIGO_X 0.05f
+#define VEL_INIMIGO_Y 0.05f
 
 #define RAIO_AGGRO_X 200.0f
 #define RAIO_AGGRO_Y 200.0f
@@ -19,13 +19,17 @@ namespace Personagens {
 		sf::Vector2f vel;
 
 		Jogador* jogador; // Ponteiro para jogador
+		sf::Clock relogio;
+		int moveAleatorio;
 
 	public:
 		Inimigo(Jogador* jogador);
 		~Inimigo();
 
-		void move();
+		
 		void perseguir(sf::Vector2f posJogador, sf::Vector2f posInimigo);
+		void movimentoAleatorio();
+		void move();
 		void executar();
 	};
 } using namespace Personagens;
